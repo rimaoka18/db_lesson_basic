d58ffb6898ff5c1aa3e8136a5651bc0aac4378e1
Ryo Imaoka <Stuntpad@gmail.com>
2024-12-30 14:54:08 +0900

    finished lesson

--- db_lesson.sql
+++ db_lesson.sql
@@ -5,9 +5,11 @@ CREATE TABLE departments (
   updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
   );
 
+-- Q2
 ALTER TABLE people
 ADD department_id INT UNSIGNED;
 
+-- Q3/Q4
 INSERT INTO departments (name) VALUES ('営業');
 INSERT INTO departments (name) VALUES ('開発');
 INSERT INTO departments (name) VALUES ('経理');
@@ -20,5 +22,64 @@ INSERT INTO people (name, email, age, gender) VALUES ('渡辺亮', 'watanaberyo@
 INSERT INTO people (name, email, age, gender) VALUES ('森田陽子', 'moritayouko@gizumo.jp', 40, 2);
 
 UPDATE people SET department_id = 1 WHERE person_id IN (1,2,3);
+UPDATE people SET department_id = 2 WHERE person_id IN (4,5,6,7);
+UPDATE people SET department_id = 3 WHERE person_id = 8;
+UPDATE people SET department_id = 4 WHERE person_id = 9;
+UPDATE people SET department_id = 5 WHERE person_id = 10;
 
--- https://github.com/rimaoka18/db_lesson_basic/pull/1
+DELETE FROM reports WHERE report_id = 11;
+
+UPDATE reports SET content = '午後からコードの修正を行いました' WHERE report_id = 2;
+UPDATE reports SET content = 'クライアントに資料を送付しました' WHERE report_id = 3;
+UPDATE reports SET content = 'チームで次のプロジェクトを計画しました' WHERE report_id = 4;
+UPDATE reports SET content = 'メール対応に多くの時間を使いました' WHERE report_id = 5;
+UPDATE reports SET content = 'データベースのテストを実施しました' WHERE report_id = 6;
+UPDATE reports SET content = '今日の業務は順調でした' WHERE report_id = 7;
+UPDATE reports SET content = '会議で新しい案が決まりました' WHERE report_id = 8;
+UPDATE reports SET content = '仕様書のレビューを進めました' WHERE report_id = 9;
+UPDATE reports SET content = '新入社員に業務の説明をしました' WHERE report_id = 10;
+
+-- Q5
+SELECT name,age FROM people WHERE gender = 1 ORDER BY age DESC;
+
+-- Q6
+SELECT
+  `name`, `email`, `age`
+FROM
+  `people`
+WHERE
+  `department_id` = 1
+ORDER BY
+  `created_at`;
+
+-- これは、peopleテーブルの中にあるdepartment_idが1に当てはまるレコードのname、email、ageのデータを表示します。created_atは、このデータがいつ作られたかを表します
+
+-- Q7
+SELECT name
+FROM people
+WHERE (gender = 2 AND age BETWEEN 20 AND 29)
+OR (gender = 1 AND age BETWEEN 40 AND 49);
+
+-- Q8
+SELECT name
+FROM *
+WHERE department_id = 1
+ORDER BY age ASC
+
+-- Q9
+SELECT AVG(age) AS average_age
+FROM people
+WHERE gender = 2 AND department_id = 2
+
+-- Q10
+SELECT p.name, d.name AS department_name, r.content
+FROM people p
+JOIN departments d ON p.department_id = d.department_id
+JOIN reports r ON p.person_id = r.person_id
+WHERE r.content IS NOT NULL;
+
+-- Q11
+SELECT p.name
+FROM people p
+LEFT JOIN reports r ON p.person_id = r.person_id
+WHERE r.content IS NULL;
